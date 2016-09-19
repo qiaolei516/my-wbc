3b3bce5042d838da6d208e2de8cb0bfc641ff8ec
qiaolei516 <qiaolei0516@163.com>
2016-09-19 22:46:56 +0800

    三级联动菜单错误

--- lesson03-联级菜单/js/lianjicaidan.js
+++ lesson03-联级菜单/js/lianjicaidan.js
@@ -1,17 +1,47 @@
 !function(window , document , $ , undefined) {
-	var arr = [
-	    {name : 北京市}
 	
+	var Datecity , Datearea;
 	
+	$('#province').on('change' , function(){
+		var val = this.value;
+		Datecity = getcitychild(val , cityData3);
+		
+		incity(Datecity , 'city');
+		$('#area').html('<option value="0">请选择</option>')
+	});
+
+
+	function incity(date , render){
+		var arr = ['<option value="0">请选择</option>'] ;
+		
+		$.each(date , function(index, obj){
+			
+		arr.push('<option value=" ' + obj.value +' ">' + obj.text + '</option>');
+			
+			
+		});
+		$('#' + render).html(arr.join(''));
+//		console.log(arr)
+		
+	}
+
+	var getcitychild=function(val , pardate){
+
+	 	var currchild = [];
+
+	 	$.each(pardate , function(index , obj) {
+
+	 		if(obj.value == val){
+	 		   currchild = obj.children;
+
+	 		    return false;
+	 		}
+	  });
+
+	 	return currchild;
+
+	 };
+
 	
-	
-	
-	
-	
-	
-	
-	]
-	
-	
-	
+	incity(cityData3 , 'province');
 }(window , document ,jQuery);
